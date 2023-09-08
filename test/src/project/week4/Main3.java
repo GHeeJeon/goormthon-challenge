4c21bf036f3be5b0bca27c178fc9b00acd37b793
전지희(풀스택 1회차) <jhjeon_99@naver.com>
2023-09-08 18:31:09 +0000

    update Q18

--- test/src/project/week4/Main3.java
+++ test/src/project/week4/Main3.java
@@ -1,14 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         // 격자의 크기와 그릴 선의 개수를 입력받는다.
-        int gridSize = scanner.nextInt();
-        int numOfLines = scanner.nextInt();
-        scanner.nextLine();  // 다음 라인으로 넘어가기 위한 코드
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int gridSize = Integer.parseInt(st.nextToken());
+        int numOfLines = Integer.parseInt(st.nextToken());
 
         // 각각의 세로선과 가로선을 저장할 2차원 배열 초기화
         long[][] verticalLines = new long[gridSize+1][gridSize+1];
@@ -19,10 +22,10 @@ public class Main {
 
         // 선의 개수만큼 반복하여 선의 정보를 입력받고 처리
         for (int s = 0; s < numOfLines; s++) {
-            int startY = scanner.nextInt();
-            int startX = scanner.nextInt();
-            String direction = scanner.next();
-            scanner.nextLine();  // 다음 라인으로 넘어가기 위한 코드
+            st = new StringTokenizer(br.readLine());
+            int startY = Integer.parseInt(st.nextToken());
+            int startX = Integer.parseInt(st.nextToken());
+            String direction = st.nextToken();
 
             // 방향에 따른 처리
             if (direction.equals("D")) {
